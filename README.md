08d90097179c40147d7233472e8a477d1eaff132
tonipone <terseo62@gmail.com>
2024-09-23 18:03:14 +0200

    Aggiunta della Class Router e Controller

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mvc_02
 tutorial MVC from youtube free
+Codice riscritto per compatibilita con PHP vers. 8.3
--- index.php
+++ index.php
@@ -1,29 +1,40 @@
 <?php
 
-session_start();
+session_start([
+    'cookie_lifetime' => 86400,  // Impostazioni opzionali della sessione
+    'read_and_close' => false,   // Evitare la scrittura se non necessario
+]);
 
-use \Core\{Config, Router,H};
+use \Core\{Config, Router, H};
 
-define('PROOT',__DIR__);
-define('DS',DIRECTORY_SEPARATOR);
+// Definizione delle costanti
+define('PROOT', __DIR__);  // Definisce la radice del progetto
+define('DS', DIRECTORY_SEPARATOR);  // Separatore di directory, dipende dal sistema operativo
 
-spl_autoload_register(function($className){
+// Autoloading per le classi
+spl_autoload_register(function($className) {
     $parts = explode('\\', $className);
-    $class = end($parts);
-    array_pop($parts);
-    $path = strtolower(implode(DS, $parts));
-    $path = PROOT . DS . $path . DS . $class . '.php';
-    if(file_exists($path)) {
-        include($path);
+    $class = array_pop($parts);  // Prende il nome della classe
+    $path = strtolower(implode(DS, $parts));  // Costruisce il percorso della cartella
+    $file = PROOT . DS . $path . DS . $class . '.php';
+
+    // Verifica che il file esista
+    if (file_exists($file)) {
+        include($file);
+    } else {
+        // Aggiunge una gestione degli errori nel caso in cui il file non venga trovato
+        throw new \Exception("File for class $className not found at $file");
     }
 });
 
+// Recupera la root directory dal file di configurazione
 $rootDir = Config::get('root_dir');
-define('ROOT',$rootDir);
+define('ROOT', $rootDir);  // Definisce la costante ROOT per il percorso root del progetto
 
+// Gestisce l'URL della richiesta
 $url = $_SERVER['REQUEST_URI'];
-$url = str_replace(ROOT,'',$url);
-$url = preg_replace('/(\?.+)/','',$url);
+$url = str_replace(ROOT, '', $url);  // Rimuove la parte ROOT dall'URL
+$url = preg_replace('/(\?.+)/', '', $url);  // Rimuove le query string dall'URL
 
-
+// Instrada la richiesta
 Router::route($url);
